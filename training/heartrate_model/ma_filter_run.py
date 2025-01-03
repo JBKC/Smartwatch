9f1891fbebb861c040ec4957f8c7525b39c54570
JBKC <jmbannon97@gmail.com>
2025-01-03 19:51:48 +0000

    filter workflow implemented using SQL

--- training/heartrate_model/ma_filter_run.py
+++ training/heartrate_model/ma_filter_run.py
@@ -63,7 +63,7 @@ def undo_normalisation(X_norm, ms, stds):
 
     return (X_norm * np.where(stds_reshaped != 0, stds_reshaped, 1)) + ms_reshaped
 
-def train_ma_filter(cur, conn, table, acts, batch_size):
+def train_ma_filter(cur, conn, acts, batch_size):
     '''
     Uses ma_filter architecture to remove motion artifacts from raw PPG signal by activity
     '''
@@ -73,93 +73,56 @@ def train_ma_filter(cur, conn, table, acts, batch_size):
         Fetch all data for given activity in batches
         '''
 
-        query = f"SELECT * FROM {table} WHERE activity = %s LIMIT %s OFFSET %s;"
+        query = f"SELECT * FROM session_data WHERE activity = %s LIMIT %s OFFSET %s;"
 
         offset = 0
         while True:
             # extract data in batches of size limit
-            cur.execute(query, (offset,))
+            cur.execute(query, (act, batch_size, offset))
             rows = cur.fetchall()
             if not rows:
                 break
-            yield rows
+
+            # save row data as dictionary
+            columns = [desc[0] for desc in cur.description]
+            batch_dict = [dict(zip(columns, row)) for row in rows]
+            yield batch_dict
+            # produces list (len=batch_size) of 2D tuples
             offset += batch_size
 
-    # fetch data by activity
+    # train by activity
     for act in acts:
+
+        # initialise activity-specific filter model
+        n_epochs = 1000
+        model = AdaptiveLinearModel()
+        optimizer = optim.SGD(model.parameters(), lr=1e-7, momentum=1e-2)
+
         print(f"Training filter for {activity_mapping[act]}...")
         for batch in fetch_activity_data(act, batch_size):
-            print(batch)
+            ppg = np.expand_dims(np.array([row['ppg'] for row in batch]), axis=1)
+            acc = np.array([row['acc'] for row in batch])
+            # print(ppg.shape)                # (batch_size, 1, 256)
+            # print(acc.shape)                # (batch_size, 3, 256)
 
+            X_BVP = []  # filtered PPG data
 
-
-    # sort all SQL data by activity into a dictionary
-    print(f'Fetching data...')
-    cur.execute(f"SELECT activity, ppg, acc FROM {table}")
-    rows = cur.fetchall()
-
-    activity_data = {}
-    for row in rows:
-        activity, ppg, acc = row
-        if activity not in activity_data:
-            activity_data[activity] = {'ppg': [], 'acc': []}
-        activity_data[activity]['ppg'].append(ppg)
-        activity_data[activity]['acc'].append(acc)
-
-    # train filter for each activity
-    for activity, data in activity_data.items():
-        print(f'Training for {activity}')
-
-        # iterate through rows and add to batches
-        # Convert lists to numpy arrays
-        ppg_data = np.array(data['ppg'])  # Shape: (n_windows, 256)
-        acc_data = np.array(data['acc'])  # Shape: (n_windows, 3, 256)
-
-        print(ppg_data.shape)
-        print(acc_data.shape)
-
-
-    # initialise CNN model
-    n_epochs = 1000
-    model = AdaptiveLinearModel()
-    optimizer = optim.SGD(model.parameters(), lr=1e-7, momentum=1e-2)
-
-    for s in sessions:
-
-        X_BVP = []  # filtered PPG data
-
-        # concatenate ppg + accelerometer signal data -> (n_windows, 4, 256)
-        X = np.concatenate((data_dict[s]['ppg'], data_dict[s]['acc']), axis=1)
-        act = data_dict[s]['activity']  # Activity labels
-
-        # find indices of activity changes (marks batches)
-        idx = np.argwhere(np.abs(np.diff(data_dict[s]['activity'])) > 0).flatten() +1
-
-        # add indices of start and end points
-        idx = np.insert(idx, 0, 0)
-        idx = np.insert(idx, idx.size, data_dict[s]['label'].shape[0])
-
-        initial_state = model.state_dict()
-
-        # iterate over activities (batches)
-        for i in range(idx.size - 1):
-
-            model.load_state_dict(initial_state)
-
-            # create batches
-            X_batch = X[idx[i]: idx[i + 1], :, :]  # splice X into current activity
+            # concatenate ppg + accelerometer signal data -> (n_windows, 4, 256)
+            X = np.concatenate((ppg, acc), axis=1)
+            # print(X.shape)                  # (batch_size, 4, 256)
 
             # batch Z-normalisation
-            X_batch, ms, stds = z_normalise(X_batch)
+            X, ms, stds = z_normalise(X)
 
-            X_batch = np.expand_dims(X_batch, axis=1)          # add channel dimension
-            X_batch = torch.from_numpy(X_batch).float()
+            X = np.expand_dims(X, axis=1)          # add channel dimension
+            X = torch.from_numpy(X).float()
 
-            # accelerometer data are inputs:
-            x_acc = X_batch[:, :, 1:, :]                 # (batch_size, 1, 3, 256)
-            # PPG data are targets:
-            x_ppg = X_batch[:, :, :1, :]                 # (batch_size, 1, 1, 256)
-            # activity-aware element
+            # accelerometer data == inputs:
+            x_acc = X[:, :, 1:, :]                 # (batch_size, 1, 3, 256)
+            print(x_acc.shape)
+            # PPG data == targets:
+            x_ppg = X[:, :, :1, :]                 # (batch_size, 1, 1, 256)
+            print(x_ppg.shape)
 
             # training loop
             for epoch in range(n_epochs):
@@ -173,53 +136,42 @@ def train_ma_filter(cur, conn, table, acts, batch_size):
                 loss.backward()
                 optimizer.step()
 
-                # print(f'Session {s}, Batch: [{i + 1}/{idx.size - 1}], '
-                #       f'Epoch [{epoch + 1}/{n_epochs}], Loss: {loss.item():.4f}')
+                print(f'Activity {activity_mapping[act]},'
+                      f'Epoch [{epoch + 1}/{n_epochs}], Loss: {loss.item():.4f}')
 
             # subtract the motion artifact estimate from raw signal to extract cleaned BVP
             with torch.no_grad():
                 x_bvp = x_ppg[:, 0, 0, :] - model(x_acc)
 
-            # get signal into original shape: (n_windows, 1, 256)
+            # get signal into original shape (n_windows, 1, 256) and de-normalise
             x_bvp = torch.unsqueeze(x_bvp, dim=1).numpy()
             x_bvp = undo_normalisation(x_bvp, ms, stds)
             x_bvp = np.expand_dims(x_bvp[:,0,:], axis=1)            # keep only BVP (remove ACC)
             X_BVP.append(x_bvp)
 
-        # add to dictionary
-        X_BVP = np.concatenate(X_BVP, axis=0)                   # shape (n_windows, 1, 256)
-        print(X_BVP.shape)
+            # save to new SQL table
+            query = """
+                INSERT INTO ma_filtered_data (row['dataset'], row['session_number'], %s
+                 row['acc'], row['activity'], row['label'])
+                VALUES (%s, %s, %s, %s, %s, %s)
+            """
 
-        # finally z-normalise each window individually to match inference
-        X_BVP = window_z_normalise(X_BVP)
+            cur.executemany(query, (X_BVP))
+            conn.commit()
 
-        ppg_filt_dict[s]['bvp'] = X_BVP
-        ppg_filt_dict[s]['acc'] = data_dict[s]['acc']
-        ppg_filt_dict[s]['label'] = data_dict[s]['label']
-        ppg_filt_dict[s]['activity'] = data_dict[s]['activity']
-
-        print(f"{s} shape: {ppg_filt_dict[s]['bvp'].shape}")
-
-    # save dictionary
-    with open('ppg_filt_dict', 'wb') as file:
-        pickle.dump(ppg_filt_dict, file)
-    print(f'Data dictionary saved to ppg_filt_dict')
+            print(f"Filtered data saved for {activity_mapping[act]}...")
 
     return
 
 def main():
 
-    def get_activities(cur, table):
-        cur.execute(f"SELECT DISTINCT activity FROM {table}")
+    def get_activities(cur):
+        cur.execute("SELECT DISTINCT activity FROM session_data")
         activities = cur.fetchall()
         return [activity[0] for activity in activities]
 
-    # filter data and add to another SQL table
+    # connect to raw database
     database = "smartwatch_raw_data_all"
-    session_table = "session_data"
-    filtered_table = "ma_filtered_data"
-
-    # connect to database
     conn = psycopg2.connect(
         dbname=database,
         user="postgres",
@@ -230,14 +182,11 @@ def main():
     cur = conn.cursor()
 
     # get unique activities
-    acts = get_activities(cur, session_table)
+    acts = get_activities(cur)
 
     # train filters & filter data
     batch_size=256
-    train_ma_filter(cur, conn, session_table, acts, batch_size)
-
-    # column_names = [desc[0] for desc in cur.description]
-    # print("Column Names:", column_names)
+    train_ma_filter(cur, conn, acts, batch_size)
 
     cur.close()
     conn.close()
